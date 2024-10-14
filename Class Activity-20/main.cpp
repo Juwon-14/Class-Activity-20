9274aab7474e49f352c2cf0ad4d0e8c12fc1cbfb
Juwon Hong <wndnjs9127@gmail.com>
2024-10-14 10:10:41 -0700

    Initial Commit

---
+++ Class Activity-20/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Class Activity-20
+//
+//  Created by 홍주원 on 10/14/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
